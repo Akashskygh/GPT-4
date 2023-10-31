bf0be521a73031295c1282456c02a5f162ed27d6
Akash Ghosh <akashghosh1998@yahoo.com>
2023-10-31 13:32:25 -0700

    Update gpt4.py

--- gpt4.py
+++ gpt4.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-API_KEY = "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"
+API_KEY = "key"
 API_ENDPOINT = "https://api.openai.com/v1/chat/completions"
 
 def generate_chat_completion(messages, model="gpt-4", temperature=1, max_tokens=None):
